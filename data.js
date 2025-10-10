765ab5114120fc6252bec0a971f2d42f7af7fc3f
Vardhini <160108780+MuttaiahgariVardhini@users.noreply.github.com>
2025-10-10 20:36:02 +0530

    Update data.js

--- data.js
+++ data.js
@@ -84,9 +84,9 @@ window.projectsData = [
 window.certificatesData = [
     {
     title: "Introduction to Generative AI",
-    org: "Microsoft",
+    org: "Simplilearn",
     year: "2025",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+    logo: "https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2022/06/simplilearn-1655124308.jpg",
     file: "assets/IntroductionToGenAI.png"
   },
   {
@@ -132,5 +132,14 @@ window.certificatesData = [
     logo: "https://tse2.mm.bing.net/th/id/OIP.DRL6d_9m5DgFlEvGcI6CNAHaEP?pid=Api&P=0&h=180", 
     file: "assets/C.jpg"
   }
+    {
+    title: "Introduction to Ms Excel",
+    org: "Simplilearn",
+    year: "2025",
+    logo: "https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2022/06/simplilearn-1655124308.jpg", 
+    file: "assets/Introduction to MsExce;.jpg"
+  }
+    
 
 ];
+
